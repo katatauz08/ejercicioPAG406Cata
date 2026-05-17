09b5b99885b94ae37bf1e40d2219df4d925cb3f3
katatauz08 <katach24@hotmail.com>
2026-05-17 10:28:49 -0500

    Ejercicio2-Entrega4-CTU

--- src/main/java/com/mycompany/ejerciciopag406/Vendedor.java
+++ src/main/java/com/mycompany/ejerciciopag406/Vendedor.java
@@ -25,15 +25,36 @@ public class Vendedor {
 }
     
     void imprimir() {
-    JOptionPane.showMessageDialog(null,"Nombre","Nombre del vendedor", JOptionPane.INFORMATION_MESSAGE);
-    JOptionPane.showMessageDialog(null,"Apellido","Apellido del vendedor", JOptionPane.INFORMATION_MESSAGE);
-    JOptionPane.showMessageDialog(null,"Edad","Edad del vendedor", JOptionPane.INFORMATION_MESSAGE);
+    JOptionPane.showMessageDialog(null,"Nombre: "+nombre,"Nombre del vendedor", JOptionPane.INFORMATION_MESSAGE);
+    JOptionPane.showMessageDialog(null,"Apellido: "+apellidos,"Apellido del vendedor", JOptionPane.INFORMATION_MESSAGE);
+    if(edad >= 18 && edad < 120){
+        JOptionPane.showMessageDialog(null,
+                "Edad: " + edad,
+                "Edad del vendedor",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+    else{
+        JOptionPane.showMessageDialog(null,
+                "Edad inválida: " + edad,
+                "Edad del vendedor",
+                JOptionPane.WARNING_MESSAGE);
+    }
 }
-    void verificarEdad(int edad) {
-    if (edad < 18) { 
-    throw new IllegalArgumentException("El vendedor debe ser mayor de 18 años.");
-    } if (edad >= 0 && edad < 120) { 
-    this.edad = edad;
-    } else throw new IllegalArgumentException("La edad no puede ser negativa ni mayor a 120.");
-}
+    public int verificarEdad(int edad){
+        if(edad<18){
+            this.edad = edad;
+            JOptionPane.showMessageDialog(null, "El vendedor debe ser mayor de 18 años.", "Error de validación de edad", JOptionPane.WARNING_MESSAGE);
+            return -1;
+        }
+        else if(edad>=18 && edad<120){
+            this.edad = edad;
+            JOptionPane.showMessageDialog(null, "Edad registrada con éxito: " + edad);
+            return edad;   
+        }
+        else{
+            this.edad = edad;
+            JOptionPane.showMessageDialog(null, "La edad no puede ser negativa ni mayor a 120", "Error de rango", JOptionPane.ERROR_MESSAGE);
+            return -1;
+        }
+    }
 }
